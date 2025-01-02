a32c6e66ded9a79e0e56a979bb8dfcf8fab96cd7
ppapill0n <facelit2002@snu.ac.kr>
2025-01-02 14:04:40 +0900

    add linking in SMod

--- modules/SMod.v
+++ modules/SMod.v
@@ -24,6 +24,63 @@ Section SMODSEM.
       List.NoDup scopes -> List.NoDup (List.map fst initial_st);
   }.
 
+  (**** Linking ****)
+  Program Definition empty : t := {|
+    scopes := [];
+    fnsems := [];
+    initial_st := [];
+  |}.
+  Next Obligation. ii; ss. Qed.
+  Next Obligation. ii; ss. Qed.
+  Next Obligation. econs. Qed.
+
+  Program Definition add ms1 ms2 : t := {|
+    scopes := ms1.(scopes) ++ ms2.(scopes);
+    fnsems := ms1.(fnsems) ++ ms2.(fnsems);
+    initial_st := ms1.(initial_st) ++ ms2.(initial_st);
+  |}.
+  Next Obligation.
+    ii. unfold fnsems_scopes in H. des_ifs.
+    rewrite alist_find_app_o in Heq. des_ifs.
+    {
+      hexploit (ms1.(well_scoped_fns) fn a).
+      { unfold fnsems_scopes. des_ifs. }
+      i. eapply in_or_app. eauto.
+    }
+    {
+      hexploit (ms2.(well_scoped_fns) fn a).
+      { unfold fnsems_scopes. des_ifs. }
+      i. eapply in_or_app. eauto.
+    }
+  Qed.
+  Next Obligation.
+    unfold state_scopes. ii. destruct ms1, ms2. ss.
+    rewrite map_app in H. apply in_or_app. apply in_app_or in H.
+    destruct H; eauto.
+  Qed.
+  Next Obligation.
+    ii. exploit nodup_app_l; eauto. i.
+    exploit nodup_app_r; eauto; i.
+    apply ms1 in x0. apply ms2 in x1.
+    assert (INCL1:= ms1.(well_scoped_init)).
+    assert (INCL2:= ms2.(well_scoped_init)).
+    revert_until ms2. unfold state_scopes.
+    generalize (initial_st ms1) as l1.
+    generalize (initial_st ms2) as l2.
+    i. revert_until l1. induction l1; ss.
+    i. econs; cycle 1.
+    {
+      eapply IHl1; eauto.
+      { eapply NoDup_cons_iff in x0. des. eauto. }
+      { ss. ii. eapply INCL1. ss. eauto. }
+    }
+    ii. rewrite map_app in H0. eapply in_app_or in H0. des.
+    { eapply NoDup_cons_iff in x0. des. eauto. }
+    eapply NoDup_app_disjoint; eauto.
+    { eapply INCL1. s. left. eauto. }
+    { eapply INCL2. rewrite - List.map_map. eapply in_map. eauto. }
+  Qed.
+
   Program Definition to_hmod (ms : t) : HModSem.t := {|
     HModSem.scopes := ms.(scopes);
     HModSem.fnsems := List.map (map_snd (λ ksb, (ksb.1, interp_sb_hp ginv stb ksb.2))) ms.(fnsems);
@@ -51,6 +108,19 @@ Section SMOD.
     modsem : Sk.t -> SModSem.t;
     sk : Sk.t;
   }.
+
+  Definition empty := {|
+    modsem := const (SModSem.empty);
+    sk := []
+  |}.
+
+  Definition add (md0 md1 : t) : t := {|
+    modsem := λ sk, SModSem.add (md0.(modsem) sk) (md1.(modsem) sk);
+    sk := Sk.add md0.(sk) md1.(sk);
+  |}.
+
+  Definition addL (ms : list t) : t :=
+    foldr add empty ms.
 
   Definition to_hmod (md:t) : HMod.t := {|
     HMod.modsem := fun sk => SModSem.to_hmod (ginv sk) (stb sk) (md.(modsem) sk);
