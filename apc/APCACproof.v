335f79a4597b21ce49d3192298e5bbaa00aa96c3
namdonghyun <cosdong7@gmail.com>
2025-03-05 15:28:31 +0900

    update apc with wsim

--- apc/APCACproof.v
+++ apc/APCACproof.v
@@ -13,22 +13,18 @@ Module APCAC. Section APCAC.
     (λ _ _ _, True)%I.
 
   (* context *)
-  Variable md: HMod.t.
+  Context (md : HMod.t).
+  Context (u : univ_id).
+  Context (spc_c spc_a spc_pure : string → option fspec).
+  Context (APCInSpcA : spc_incl APCA.Spc spc_a).
+  Context (PureInSpcA : spc_sub spc_pure spc_a).
+  Context (PureIsPure :
+            ∀ fn pfsp, 
+            spc_pure fn = Some pfsp 
+              → ∃ scopes, find_body md fn = Some (pure_specbody scopes u spc_a pfsp)).
 
-  Variable u : univ_id.
-  Variable SpcC : string → option fspec.
-  Variable SpcA : string → option fspec.
-  Variable SpcPure : string → option fspec.
-
-  Hypothesis APCInSpcA : spc_incl APCA.Spc SpcA.
-  Hypothesis PureInSpcA : spc_sub SpcPure SpcA.
-  Hypothesis PureIsPure :
-    ∀ fn pfsp, 
-      SpcPure fn = Some pfsp 
-      → ∃ scopes, find_body md fn = Some (pure_specbody scopes u SpcA pfsp).
-
-  Local Definition APCC := (APCC.t u SpcC).
-  Local Definition APCA := (APCA.t u SpcPure SpcA).
+  Local Definition APCC := (APCC.t u spc_c).
+  Local Definition APCA := (APCA.t u spc_pure spc_a).
   Local Definition APCCMod := (APCC ★ md).
   Local Definition APCAMod := (APCA ★ md).
   Local Definition IstFull := (IstProd (IstSB APCC.(HMod.scopes) Ist) IstEq).
@@ -38,73 +34,85 @@ Module APCAC. Section APCAC.
   Lemma simF_apc :
     HSim.sim_fun open APCCMod APCAMod IstFull APCName.apc.
   Proof.
-    init_simF.
-    steps_l. iDestruct "ASM" as "%"; des; subst.
-    steps_r. force_r q. force_r (q↑). force_r. iSplitR; et. hss. steps_r.
+    winit_simF u 0.
+    (* init_simF. *)
+    wsteps_l. iDestruct "ASM" as "%"; des; subst.
+    wsteps_r. wforce_r q. wforce_r (q↑). wforce_r. iSplitR; et. hss. wsteps_r.
 
     (* normalize itree - remove all interpretations and sandboxes except APC *)
     (* SRC *)
-    bind_expand_l.
+    wbind_expand_l.
     (* TGT *)
-    bind_expand_r. steps_r.
+    wbind_expand_r. wsteps_r.
 
     (* add meaningless return in src *)
     set (itr:=(ITree.bind _ _)).
-    eapply isim_congruence_src.
+    eapply wsim_congruence_src.
     { instantiate (1:=Ret ();;; itr). rewrite bind_ret_l. refl. }
 
-    iApply isim_bind. iSplitL; cycle 1.
+    iApply wsim_bind. iSplitL; cycle 1.
     { iIntros (? ? ? ? ?) "R".
       instantiate (1:=(λ nths '(st_src, _) '(st_tgt, _), IstFull nths st_src st_tgt)%I).
-      subst itr. steps_r. force_l. steps_l. forces_l. iSplitR; et. step. iSplit; et. }
+      subst itr. wsteps_r. wforce_l. wsteps_l. wforces_l. iSplitR; et. wstep. iSplit; et. }
     clear itr.
 
     (* well founded induction on depth ordinal *)
-    iApply isim_reset. iStopProof. 
+    iApply wsim_reset. iStopProof. 
     generalize scopes st_tgt st_src nths. revert q0. pattern q. set (GOAL:=λ _, _).
     revert q. apply (well_founded_induction Ord.lt_well_founded).
     i. subst GOAL. ss. iIntros (? ? ? ? ?) "IST".
 
     (* well founded induction on width ordinal *)
-    iApply isim_reset. iStopProof. 
+    iApply wsim_reset. iStopProof. 
     generalize st_tgt0 st_src0 nths0. pattern q0. set (GOAL:=λ _, _).
     revert q0. apply (well_founded_induction Ord.lt_well_founded).
     i. subst GOAL. ss. iIntros (? ? ?) "IST".
-    
-    rewrite unfold_APC. steps_r. des_ifs. { step. iFrame. }
-    steps_r. des.
+
+    rewrite unfold_APC. wsteps_r. des_ifs. { wstep. iFrame. }
+    wsteps_r.
+
+    (* manually do what wsim_unwrapN_tgt does *)
+    unfold unwrapN. des_ifs. 2:{ unfold triggerNB. wsteps_r. des_ifs. }
+    rename Heq into G, f into q3. wsteps_r. 
     
     (* inlining *)
     unfold is_Some in *. des. dup grt. apply PureInSpcA in grt. rewrite grt in G. inv G; ss.
     apply PureIsPure in grt1. ss. destruct grt1. unfold find_body in H1.
-    hide_itree_l; prep; iApply isim_inline_tgt_simpl.
+    hide_itree_l; prep; iApply wsim_inline_tgt.
     { subst FLT. rewrite map_app. apply alist_find_comm.
       { rewrite map_app. rewrite !map_fst_map_map_snd_refl.
         apply nodup_comm. rewrite -map_app. eauto. }
       apply alist_find_app. et. }
     s; show_itree.
 
-    unfold pure_specbody, interp_sb_hp, HoareFun. steps_r.
-    force_r q4. forces_r. iSplitR "IST"; et.
+    unfold pure_specbody, interp_sb_hp, HoareFun. wsteps_r.
+    wforce_r q4. wforces_r. iSplitR "IST"; et.
 
-    steps_r. unfold pure_body. steps_r. hss. iDestruct "GRT" as "%". hss.
+    wsteps_r. unfold pure_body.
+    (* manually do what wsim_unwrapN_tgt does *)
+    wstep_r. hss. wsteps_r. rename q7 into q8, q6 into q7.
+
+    wsteps_r. iDestruct "GRT" as "%". hss.
 
     (* inlining *)
-    inline_r. steps_r. force_r q7. forces_r. iSplitR; et. steps_r.
+    winline_r. wsteps_r. wforce_r q7. wforces_r. iSplitR; et. wsteps_r.
+
+    (* manually do what wsim_unwrapN_tgt does *)
+    hss. wsteps_r.
 
     (* normalize itree *)
-    bind_expand_r.
+    wbind_expand_r.
 
     (* add meaningless return in src *)
-    set (itr:=(λ _: unit, _)). steps_r.
-    eapply isim_congruence_src.
+    set (itr:=(λ _: unit, _)). wsteps_r.
+    eapply wsim_congruence_src.
     { instantiate (1:=Ret ();;; Ret ()). grind. }
 
-    iApply isim_bind. iSplitL; cycle 1.
+    iApply wsim_bind. iSplitL; cycle 1.
     { iIntros (? ? ? ? ?) "R". instantiate (1:=(λ nths '(st_src, _) '(st_tgt, _), IstFull nths st_src st_tgt)%I).
-      subst itr. steps_r. forces_r. iSplitL "GRT"; et.
-      steps_r. forces_r. iSplitL "GRT"; et. steps_r. iApply isim_reset. iStopProof. eapply H0; et. }
-    clear itr. iApply isim_reset. iStopProof. eapply H; et. hss.
+      subst itr. wsteps_r. wforces_r. iSplitL "GRT"; et.
+      wsteps_r. wforces_r. iSplitL "GRT"; et. wsteps_r. iApply wsim_reset. iStopProof. eapply H0; et. }
+    clear itr. iApply wsim_reset. iStopProof. eapply H; et.
     Unshelve. all: ss.
   Qed.
 
@@ -117,18 +125,20 @@ Module APCAC. Section APCAC.
       iPureIntro. split; prove_scope.
     - eapply simF_apc; eauto.
   Qed.
+End APCAC.
 
-  Theorem correct :
-    ∀ Pmd, ctx_refines
-      (APCCMod, Pmd)
-      (APCAMod, Pmd).
-  Proof.
-    intro Pmd.
-    rewrite -(hmod_addc_empty_l APCCMod) -(hmod_addc_empty_l APCAMod).
-    rewrite -(hmod_add_empty_r APCCMod) -(hmod_add_empty_r APCAMod).
-    eapply ctxr_compose_hor; [|refl].
-    eapply main_adequacy.
-    apply sim.
-  Qed.
+Section wctxr.
+  Context `{!invG α Σ Γ, !subG Γ Σ, !sinvG Σ Γ α β τ}.
 
-End APCAC. End APCAC.
+  Definition wctxr (md : HMod.t) (u : univ_id) (spc_c spc_a spc_pure : string → option fspec)
+      (APCInSpcA : spc_incl APCA.Spc spc_a)
+      (PureInSpcA : spc_sub spc_pure spc_a)
+      (PureIsPure : 
+                  ∀ fn pfsp, 
+                    spc_pure fn = Some pfsp 
+                    → ∃ scopes, find_body md fn = Some (pure_specbody scopes u spc_a pfsp)) :
+    ctx_refines
+      ((APCC.t u spc_c)           ★ md, emp%I)
+      ((APCA.t u spc_pure spc_a)  ★ md, emp%I).
+  Proof. eapply main_adequacy, sim; eauto. Qed.
+End wctxr. End APCAC.
--- apc/NormITree.v
+++ apc/NormITree.v
@@ -1,4 +1,5 @@
 Require Import CRIS.
+Require Import wsim.
   
 (* Lemma isim_congruence f fl fr Ist tid is_closed r g {Rs Rt} RR ps pt nths st_src st_tgt s0 s1 t0 t1
   (EQS: s0 = s1)
@@ -24,6 +25,44 @@ Proof.
   subst. eauto.
 Qed.
 
+Lemma wsim_congruence_tgt `{!invG α Σ Γ, !subG Γ Σ, !sinvG Σ Γ α β τ}
+  f fl fr Ist u0 u1 cP is_closed r g Rs Rt RR ps pt nths sti_src st_tgt t0 t1
+  (EQ: t0 = t1):
+  f (wsim fl fr Ist is_closed u0 u1 cP r g Rs Rt RR ps pt nths sti_src (st_tgt, t0) ) ->
+  (f (wsim fl fr Ist is_closed u0 u1 cP r g Rs Rt RR ps pt nths sti_src (st_tgt, t1) ): Prop).
+Proof.
+  subst. eauto.
+Qed.
+
+Lemma wsim_congruence_src `{!invG α Σ Γ, !subG Γ Σ, !sinvG Σ Γ α β τ}
+  f fl fr Ist u0 u1 cP is_closed r g Rs Rt RR ps pt nths st_src sti_tgt t0 t1
+  (EQ: t0 = t1):
+  f (wsim fl fr Ist is_closed u0 u1 cP r g Rs Rt RR ps pt nths (st_src, t0) sti_tgt) ->
+  (f (wsim fl fr Ist is_closed u0 u1 cP r g Rs Rt RR ps pt nths (st_src, t1) sti_tgt): Prop).
+Proof.
+  subst. eauto.
+Qed.
+
+Lemma wsim_bind `{!invG α Σ Γ, !subG Γ Σ, !sinvG Σ Γ α β τ}
+  fl fr Ist is_closed u0 u1 cP r g ps pt {Rs Rt Qs Qt} RR QQ nths st_src st_tgt i_src i_tgt k_src k_tgt :
+  wsim fl fr Ist None u0 u1 cP r g Qs Qt QQ ps pt nths (st_src, i_src) (st_tgt, i_tgt)
+  ∗ (∀ nths0 st_src0 ret_src st_tgt0 ret_tgt,
+      QQ nths0 (st_src0, ret_src) (st_tgt0, ret_tgt)
+      -∗ wsim fl fr Ist is_closed u0 u1 cP r g Rs Rt RR false false nths0 (st_src0, k_src ret_src) (st_tgt0, k_tgt ret_tgt))%I
+  ⊢ (wsim fl fr Ist is_closed u0 u1 cP r g Rs Rt RR ps pt nths (st_src, i_src >>= k_src) (st_tgt, i_tgt >>= k_tgt)).
+Proof.
+  rewrite wsim.wsim_eq /wsim.wsim_def /wsim.wsim_pre. des_ifs.
+  { iIntros "[W SIM] INV". iApply isim_bind.
+    iSplitL "W". { iApply "W"; eauto. }
+    iIntros (? ? ? ? ?) "Q". iApply ("SIM" with "Q"). iFrame. }
+  { iIntros "[W SIM] INV". iApply isim_bind.
+    iSplitL "W". { iApply "W"; eauto. }
+    iIntros (? ? ? ? ?) "Q". iApply ("SIM" with "Q"). iFrame. }
+  { iIntros "[W SIM] INV". iApply isim_bind.
+    iSplitL "W". { iApply "W"; eauto. }
+    iIntros (? ? ? ? ?) "Q". iApply ("SIM" with "Q"). iFrame. }
+Qed.
+
 Lemma bind_equal {E : Type → Type} {T U : Type} t0 k0 t1 k1:
   t0 = t1 → k0 = k1 → @ITree.bind E T U t0 k0 = @ITree.bind E T U t1 k1.
 Proof.
@@ -47,5 +86,19 @@ Ltac bind_expand_l :=
   hide_itree_r;
   rewrite ?SRed.bind ?SBRed.bind ?bind_bind;
   eapply isim_congruence_src;
+  _bind_expand;
+  show_itree.
+
+Ltac wbind_expand_r := 
+  hide_itree_l;
+  rewrite ?PRed.bind ?HRed.bind ?bind_bind;
+  eapply wsim_congruence_tgt;
+  _bind_expand;
+  show_itree.
+
+Ltac wbind_expand_l := 
+  hide_itree_r;
+  rewrite ?SRed.bind ?HRed.bind ?bind_bind;
+  eapply wsim_congruence_src;
   _bind_expand;
   show_itree.
